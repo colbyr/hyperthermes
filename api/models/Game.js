24673dfe33a5279b7b868b36d4c427af570832a7
Colby Rabideau <idontknow5691@gmail.com>
2013-05-03 20:57:58 -0400

    game model

---
+++ api/models/Game.js
@@ -0,0 +1,14 @@
+/*---------------------
+  :: Game
+  -> model
+---------------------*/
+module.exports = {
+
+  attributes  : {
+    'profit': 'INTEGER',
+    'w-cost': 'INTEGER',
+    'j-cost': 'INTEGER',
+    'd-cost': 'INTEGER'
+  }
+
+};
